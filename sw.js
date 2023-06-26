0b34b774c79f047646ab890b08d56fd9bef8c2eb
AkileshRao <akileshrao19@gmail.com>
2023-06-26 15:10:42 +0530

    Cached resources

--- sw.js
+++ sw.js
@@ -1,2 +1,41 @@
-console.log("Hello world")
-console.log(self)
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open("v1");
+    await cache.addAll(resources);
+}
+
+const cacheMatch = async (request, preloadResponsePromise) => {
+    const cachedResponse = await caches.match(request)
+    if (cachedResponse) return cachedResponse
+    try {
+        const networkResponse = await fetch(request)
+        const cache = await caches.open("v1")
+        await cache.put(request, networkResponse.clone())
+        return networkResponse
+    } catch (err) {
+        return new Response("Response not found!")
+    }
+}
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(addResourcesToCache([
+        "/",
+        "/app.js",
+        "/index.html",
+        "/contact.html",
+        "/profile.html",
+        "/images/contactus.png",
+        "/images/home.jpg",
+        "/images/profile.jpg"
+    ]))
+})
+
+self.addEventListener("activate", event => {
+    event.waitUntil(async () => {
+        if (self.registration.navigationPreload) {
+            await self.registration.navigationPreload.enable();
+        }
+    })
+})
+self.addEventListener("fetch", (event) => {
+    event.respondWith(cacheMatch(event.request))
+})
